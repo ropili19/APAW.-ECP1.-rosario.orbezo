9e22d1acd62acc79e98ce50569eff819e7d4d3c9
ropili19 <rosariopili26@gmail.com>
2018-10-15 23:29:29 +0200

    Luggage

---
+++ src/main/java/entities/Luggage.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Luggage {
+    private String id;
+    private boolean ishand_lugagge;
+
+    public Luggage(String id, boolean ishand_lugagge) {
+        this.id = id;
+        this.ishand_lugagge = ishand_lugagge;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isIshand_lugagge() {
+        return ishand_lugagge;
+    }
+
+    public void setIshand_lugagge(boolean ishand_lugagge) {
+        this.ishand_lugagge = ishand_lugagge;
+    }
+}
